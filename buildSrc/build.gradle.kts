9c52e979774d77ab6886b77b08551898a14a4f86
Kneelawk <kneelawk@gmail.com>
2024-05-05 11:27:32 -0700

    Begin on convention plugins

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java-gradle-plugin")
+    kotlin("jvm") version "1.9.20"
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    maven("https://maven.fabricmc.net/") { name = "Fabric" }
+    maven("https://maven.architectury.dev/") { name = "Architectury" }
+    maven("https://maven.quiltmc.org/repository/release") { name = "Quilt" }
+    maven("https://maven.neoforged.net/releases/") { name = "NeoForged" }
+    maven("https://kneelawk.com/maven") { name = "Kneelawk" }
+}
