eb40c321dce3310fd1ee226b7be06cac79626e1d
Kneelawk <kneelawk@gmail.com>
2024-05-20 01:41:29 -0700

    Add package info for util package

---
+++ xplat/src/main/java/com/kneelawk/codextra/api/util/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package contains general utilities.
+ */
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.kneelawk.codextra.api.util;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
