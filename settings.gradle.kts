24484f8c4aa172fd57766d95dbdb2d186b20ff17
Kneelawk <kneelawk@gmail.com>
2024-05-31 13:23:41 -0700

    Use published plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,13 +6,19 @@ pluginManagement {
         maven("https://maven.architectury.dev/") { name = "Architectury" }
         maven("https://maven.quiltmc.org/repository/release") { name = "Quilt" }
         maven("https://maven.neoforged.net/releases/") { name = "NeoForged" }
-        maven("https://kneelawk.com/maven") { name = "Kneelawk" }
+        maven("https://maven.kneelawk.com/releases/") { name = "Kneelawk" }
     }
     plugins {
         val architectury_loom_version: String by settings
         id("dev.architectury.loom") version architectury_loom_version
         val remapcheck_version: String by settings
         id("com.kneelawk.remapcheck") version remapcheck_version
+        val versioning_version: String by settings
+        id("com.kneelawk.versioning") version versioning_version
+        val kpublish_version: String by settings
+        id("com.kneelawk.kpublish") version kpublish_version
+        val submodule_version: String by settings
+        id("com.kneelawk.submodule") version submodule_version
     }
 }
 
